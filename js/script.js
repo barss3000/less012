1b882eccc2ccbabc6b3f70b720c3098aaeeab1ec
Den <barss3000>
2021-07-22 22:26:00 +1000

    index.html /js script 3

--- js/script.js
+++ js/script.js
@@ -23,3 +23,23 @@
 
 'use strict';
 
+// ----1-----
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// -----2-----
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+// -----3------
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('А какой ещё?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
